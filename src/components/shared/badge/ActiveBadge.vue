eb53953c505c79ec156232d5dad70dbc065f3d8e
JiaYu <alice49885@gmail.com>
2024-03-08 01:20:35 +0800

    feat: 🎸 add components

---
+++ src/components/shared/badge/ActiveBadge.vue
@@ -0,0 +1,21 @@
+<template>
+  <span v-if="active"
+  :class="`inline-flex items-center gap-1 px-2 py-1 rounded-lg text-sm text-nowrap
+  bg-lime-50 text-lime-700`">
+    <span :class="`hidden md:inline-block h-1.5 w-1.5 rounded-full bg-lime-700`"></span>
+    <slot name="trueMsg"></slot>
+  </span>
+  <span v-else
+  :class="`inline-flex items-center gap-1 px-2 py-1 rounded-lg text-sm text-nowrap
+  bg-orange-50 text-orange-700`">
+    <span :class="`hidden md:inline-block h-1.5 w-1.5 rounded-full bg-orange-700`"></span>
+    <slot name="falseMsg"></slot>
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    active: [Boolean, Number],
+  },
+};
+</script>
---
+++ src/components/shared/form/ChecksRadio.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center">
+    <slot inputClass="rounded-sm border-stone-400 bg-primary-50 text-primary-800 mr-2
+    focus:ring
+    focus:ring-primary-800 focus:ring-opacity-50 focus:border-primary-80"></slot>
+    <label :for="labelFor">{{ label }}</label>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    labelFor: String,
+    label: String,
+  },
+};
+</script>
