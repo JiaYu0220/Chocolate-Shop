cd579d03e431bbeb74e5fa48b20d8fb48f858d93
JiaYu <alice49885@gmail.com>
2024-03-24 23:03:11 +0800

    style: 💄 change hover animation

--- src/components/userPages/product/ProductCard.vue
+++ src/components/userPages/product/ProductCard.vue
@@ -1,12 +1,21 @@
 <template>
   <RouterLink :to="`/product/${product.id}`"
   class="flex flex-col bg-primary-50 rounded-md border border-primary-300
-  overflow-hidden h-full group/product">
+  overflow-hidden h-full transition-all group
+  hover:shadow-md" @mouseenter="handleEnter" @mouseleave="handleLeave">
     <div class="h-64 overflow-hidden">
-      <img loading="lazy"
-      class="object-cover h-full w-full
-      group-hover/product:brightness-75 transition-all"
-      :src="product.imageUrl" alt="產品圖">
+      <swiper-container class="h-full" ref="imageSwiper" effect="fade">
+        <swiper-slide class="h-full" lazy="true">
+          <img loading="lazy"
+          class="object-cover h-full w-full"
+          :src="this.product.imageUrl" alt="產品圖" />
+        </swiper-slide>
+        <swiper-slide lazy="true" v-if="Array.isArray(this.product.imagesUrl)">
+          <img loading="lazy"
+          class="object-cover h-full w-full"
+          :src="this.product.imagesUrl[0]" alt="產品圖" />
+        </swiper-slide>
+      </swiper-container>
     </div>
     <div class="p-5 grow flex flex-col">
       <div class="flex justify-between items-center">
@@ -18,11 +27,12 @@
       </div>
       <p class="font-bold text-lg text-primary-800 mb-1">{{ product.title }}</p>
       <p class="text-sm mb-3">{{ product.unit }}</p>
-      <p class="mb-3">
-        <span class="text-2xl text-primary-800 mr-1">
+      <p class="mb-3 flex flex-wrap">
+        <span class="text-lg md:text-2xl text-primary-800 mr-1">
         <template v-if="product.price !== product.origin_price">特惠</template>
         NT$ {{ product.price }}</span>
-        <del v-if="product.price !== product.origin_price">NT$ {{ product.origin_price }}</del>
+        <del class="text-sm md:text-base"
+        v-if="product.price !== product.origin_price">NT$ {{ product.origin_price }}</del>
       </p>
       <LoadingBtn class="btn btn-primary w-full mt-auto"
       @click.prevent="postCart(product.id)"
@@ -33,7 +43,13 @@
     </div>
   </RouterLink>
 </template>
+<style>
+swiper-container::part(container){
+  --swiper-theme-color: #361707;
+}
+</style>
 <script>
+import { register } from 'swiper/element/bundle';
 import { mapActions, mapState } from 'pinia';
 import loadingStore from '@/stores/loadingStore';
 import cartStore from '@/stores/cartStore';
@@ -50,13 +66,22 @@ export default {
   data() {
     return {
       isFavorite: false,
+      // image: '',
+      // isHover: false,
     };
   },
   mounted() {
+    register();
     this.isFavorite = this.favorites.includes(this.product.id);
+    // this.image = this.product.imageUrl;
   },
-
   methods: {
+    handleEnter() {
+      this.$refs.imageSwiper.swiper.slideNext();
+    },
+    handleLeave() {
+      this.$refs.imageSwiper.swiper.slidePrev();
+    },
     toggleFavorite() {
       this.isFavorite = !this.isFavorite;
       this.handleFavorite(this.isFavorite, this.product.id);
