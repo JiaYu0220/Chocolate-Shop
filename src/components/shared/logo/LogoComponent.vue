48057e2c5dd97ee339bc4214d08e99d10d31fb14
JiaYu <alice49885@gmail.com>
2024-03-28 13:55:21 +0800

    refactor: 💡 class

--- src/components/shared/logo/LogoComponent.vue
+++ src/components/shared/logo/LogoComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 :class="`font-zenMaru text-primary-800 text-center text-nowrap logoShadow`">
+  <h1 class="font-zenMaru text-primary-800 text-center text-nowrap logoShadow">
      可_可
   </h1>
 </template>
