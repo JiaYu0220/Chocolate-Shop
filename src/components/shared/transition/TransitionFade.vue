504a37ee5e7c64436623123a4551b4209b9e29e8
JiaYu <alice49885@gmail.com>
2024-04-08 14:02:34 +0800

    feat: 🎸 vue router transition fade

--- src/components/shared/transition/TransitionFade.vue
+++ src/components/shared/transition/TransitionFade.vue
@@ -4,7 +4,18 @@
     enter-active-class="animate__animated animate__fadeIn animate__faster"
     leave-active-class="animate__animated animate__fadeOut animate__faster"
     @after-leave="$emit('onAfterLeave')"
+    :mode="mode"
   >
     <slot></slot>
   </Transition>
 </template>
+<script>
+export default {
+  props: {
+    mode: {
+      type: String,
+      default: 'default',
+    },
+  },
+};
+</script>
