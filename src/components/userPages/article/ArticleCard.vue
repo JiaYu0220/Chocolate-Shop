9747ec814aa7bde1f9b4364ca8d434d33a990827
JiaYu <alice49885@gmail.com>
2024-04-15 01:07:57 +0800

    style: 💄 article description

--- src/components/userPages/article/ArticleCard.vue
+++ src/components/userPages/article/ArticleCard.vue
@@ -12,7 +12,7 @@
         class="absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center
           bg-primary-800/30 p-8 opacity-0 transition-opacity duration-300 group-hover:opacity-100"
       >
-        <p class="mb-4 line-clamp-3 text-justify text-primary-50 sm:text-lg">
+        <p class="mb-4 line-clamp-3 text-justify text-primary-50 hidden sm:block sm:text-lg">
           {{ article.description }}
         </p>
         <div class="btn bg-primary-50">了解更多</div>
@@ -25,6 +25,9 @@
     <h4 class="mb-2 text-lg font-bold text-primary-800 sm:mb-4 sm:text-xl xl:text-2xl">
       {{ article.title }}
     </h4>
+    <p class="mb-2 line-clamp-3 text-justify sm:hidden">
+      {{ article.description }}
+    </p>
     <ul v-if="Array.isArray(article.tag)" class="flex gap-1 text-sm sm:text-base">
       <li
         v-for="item in article.tag"
