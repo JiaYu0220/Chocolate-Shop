838bb34e512e8e946af23e4f00d7160c6a95fe1b
JiaYu <alice49885@gmail.com>
2024-04-17 15:01:30 +0800

    Updates

--- assets/validateStore-Made9C9g.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-BVq8seMw.js";const n=t("validateStore",{actions:{isPhone(e){return/^(09)[0-9]{8}$/.test(e)?!0:"須為有效的手機號碼"}}});export{n as v};
---
+++ assets/validateStore-rRak3RNs.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-C2aWBhNW.js";const n=t("validateStore",{actions:{isPhone(e){return/^(09)[0-9]{8}$/.test(e)?!0:"須為有效的手機號碼"}}});export{n as v};
